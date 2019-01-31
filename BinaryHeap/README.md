c0e0a0f9910ce3852c4b12ef5ee3941079726302
Georgy Guminov <gogagum@gmail.com>
2019-01-31 15:09:03 +0300

    Ignoring .idea/ path

--- BinaryHeap/README.md
+++
@@ -1,2 +0,0 @@
-# bin-heap
-Binomial heap library. Made as a part of homework for Algorithms and Data Structures at DIHT MIPT.
--- BinaryHeap/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gogagum/CLionProjects/Lab/BinaryHeap/lib/googletest-master/googletest
-# Build directory: /home/gogagum/CLionProjects/Lab/BinaryHeap/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
