31a9703137695f8105c1adac8dcc58d694e97dec
Georgy Guminov <gogagum@gmail.com>
2019-01-31 15:02:18 +0300

    Added some files to gitignore

--- BinomialHeap/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gogagum/CLionProjects/Lab/BinomialHeap/lib/googletest-master/googlemock
-# Build directory: /home/gogagum/CLionProjects/Lab/BinomialHeap/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
